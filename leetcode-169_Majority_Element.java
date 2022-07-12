810f704955dded60598a38aa59bbcffaeb3690bb
Dhruv Solanki <71400881+Dhruvsolanki345@users.noreply.github.com>
2022-07-12 14:21:50 +0530

    Create leetcode-169_Majority_Element.java

---
+++ leetcode-169_Majority_Element.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/majority-element/
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer curr = null;
+        
+        for(int num: nums){
+            if(count == 0) curr = num;
+            
+            count += (curr == num) ? 1 : -1;
+        }
+        
+        return curr;
+    }
+}

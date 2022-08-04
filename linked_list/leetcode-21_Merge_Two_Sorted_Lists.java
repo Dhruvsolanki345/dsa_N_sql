42bd00a19689bd8eb52cb66bb861ccc1133bc53a
Dhruv Solanki <71400881+Dhruvsolanki345@users.noreply.github.com>
2022-08-04 13:05:32 +0530

    Update leetcode-21_Merge_Two_Sorted_Lists.java

--- linked_list/leetcode-21_Merge_Two_Sorted_Lists.java
+++ linked_list/leetcode-21_Merge_Two_Sorted_Lists.java
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/merge-two-sorted-lists/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -15,41 +16,28 @@ class Solution {
         else if(list2 == null) return list1;
         else if(list1 == null) return list2;
         
-        ListNode curr1 = list1, curr2 = list2, head, newCurr;
+        ListNode curr1 = list1, curr2 = list2;
+        ListNode head = new ListNode(-1);
+        ListNode newCurr = head;
         
-        if(curr1.val < curr2.val) {
-            head = curr1;
-            curr1 = curr1.next;
-        } else {
-            head = curr2;
-            curr2 = curr2.next;
-        }
-        
-        newCurr = head;
-        
-        while(curr1 != null && curr2 != null){
-            if(curr1.val < curr2.val) {
+        while(curr1 != null || curr2 != null){
+            if(curr1 == null) {
+                newCurr.next = curr2;
+                curr2 = curr2.next;
+            } else if(curr2 == null) {
+                newCurr.next = curr1;
+                curr1 = curr1.next;
+            } else if(curr1.val < curr2.val) {
                 newCurr.next = curr1;
                 curr1 = curr1.next;
             } else {
                 newCurr.next = curr2;
                 curr2 = curr2.next;
             }
+            
             newCurr = newCurr.next;
         }
         
-        while(curr1 != null){
-            newCurr.next = curr1;
-            curr1 = curr1.next;
-            newCurr = newCurr.next;
-        }
-        
-        while(curr2 != null){
-            newCurr.next = curr2;
-            curr2 = curr2.next;
-            newCurr = newCurr.next;
-        }
-        
-        return head;
+        return head.next;
     }
 }

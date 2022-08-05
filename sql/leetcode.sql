7a457fd1009e4a2c0cd359f6e00044e145a9e86f
Dhruv Solanki <71400881+Dhruvsolanki345@users.noreply.github.com>
2022-08-05 19:01:26 +0530

    Add leetcode-183. Customers Who Never Order

--- sql/leetcode.sql
+++ sql/leetcode.sql
@@ -23,3 +23,17 @@ select product_id from products where low_fats = "Y" and recyclable = "Y"
 select name from customer where referee_id <> 2 or referee_id is null;
 -- using !=
 select name from customer where referee_id != 2 or referee_id is null;
+
+
+-- https://leetcode.com/problems/customers-who-never-order/
+-- 183. Customers Who Never Order
+
+-- using subquery and not in
+select name as 'customers' from customers where id not in (select customerid from orders);
+
+-- using left join
+select name as 'customers' 
+from customers c 
+left join orders o
+on c.id = o.customerid
+where o.customerid is null;

dd321bf69f0079babd9fca7b1921465a3d2bb472
Dhruv Solanki <71400881+Dhruvsolanki345@users.noreply.github.com>
2022-08-05 18:48:39 +0530

    Add leetcode-1757. Recyclable and Low Fat Products

--- sql/leetcode.sql
+++ sql/leetcode.sql
@@ -1,4 +1,5 @@
 -- https://leetcode.com/problems/big-countries/
+-- 595. Big Countries
 
 -- using or
 select name, population, area from world where area >= 3000000 or population >= 25000000
@@ -7,3 +8,9 @@ select name, population, area from world where area >= 3000000 or population >= 
 select name, population, area from world where area >= 3000000 
 union
 select name, population, area from world where population >= 25000000
+
+
+-- 1757. Recyclable and Low Fat Products
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+select product_id from products where low_fats = "Y" and recyclable = "Y"

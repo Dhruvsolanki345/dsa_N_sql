e563fa6ca1ad6a20591c45a5efa851f88abdaa20
Dhruv Solanki <71400881+Dhruvsolanki345@users.noreply.github.com>
2022-08-13 20:12:45 +0530

    Add leetcode-1148. Article Views I

--- sql/leetcode-1.sql
+++ sql/leetcode-1.sql
@@ -14,3 +14,11 @@ from visits v left join transactions t
 on v.visit_id = t.visit_id
 where transaction_id is null
 group by customer_id;
+
+
+-- https://leetcode.com/problems/article-views-i/
+-- 1148. Article Views I
+
+select distinct viewer_id as id from views
+where viewer_id = author_id
+order by viewer_id;

0db8e7658ab24c5fe0202b49c346420fa86a43d3
Dhruv Solanki <71400881+Dhruvsolanki345@users.noreply.github.com>
2022-08-13 20:08:42 +0530

    Add leetcode-1581. Customer Who Visited but Did Not Make Any Transactions

--- sql/leetcode-1.sql
+++ sql/leetcode-1.sql
@@ -4,3 +4,13 @@
 select firstname, lastname, city, state 
 from person p left join address a
 on p.personid = a.personid;
+
+
+-- https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+
+select customer_id, count(v.visit_id) as count_no_trans
+from visits v left join transactions t
+on v.visit_id = t.visit_id
+where transaction_id is null
+group by customer_id;

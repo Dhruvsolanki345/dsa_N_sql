0017814be3387d88c320a62a49ad7b9e2c534217
Dhruv Solanki <71400881+Dhruvsolanki345@users.noreply.github.com>
2022-08-13 20:27:52 +0530

    Add leetcode-607. Sales Person

--- sql/leetcode-1.sql
+++ sql/leetcode-1.sql
@@ -30,3 +30,15 @@ order by viewer_id;
 select w1.id as id 
 from weather w1 join weather w2
 on datediff(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
+
+
+-- https://leetcode.com/problems/sales-person/
+-- 607. Sales Person
+
+select s.name from salesperson s
+where s.sales_id not in (
+    select o.sales_id from orders o
+    join company c
+    on o.com_id = c.com_id
+    where c.name = 'RED'
+);

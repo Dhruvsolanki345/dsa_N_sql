2bcba39174ba18155c9c19cd3f778017a10380ea
Dhruv Solanki <71400881+Dhruvsolanki345@users.noreply.github.com>
2022-08-17 00:36:03 +0530

    Create leetcode-429_N-ary_Tree_Level_Order_Traversal.java

---
+++ tree/leetcode-429_N-ary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/n-ary-tree-level-order-traversal/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+        if(root == null) return res;
+        
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> lvl = new ArrayList<>();
+            
+            while(size-- > 0) {
+                Node curr = q.poll();
+                lvl.add(curr.val);
+                
+                for(Node child: curr.children) {
+                    q.offer(child);
+                }
+            }
+            
+            res.add(lvl);
+        }
+        
+        return res;
+    }
+}

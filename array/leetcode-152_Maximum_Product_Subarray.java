166f8b67836a7563411d7783b910306da3d5e693
Dhruv Solanki <71400881+Dhruvsolanki345@users.noreply.github.com>
2022-08-23 20:01:07 +0530

    Add leetcode-152_Maximum_Product_Subarray.java 2nd approach

--- array/leetcode-152_Maximum_Product_Subarray.java
+++ array/leetcode-152_Maximum_Product_Subarray.java
@@ -1,5 +1,8 @@
 // https://leetcode.com/problems/maximum-product-subarray/
 
+// Reference: https://takeuforward.org/data-structure/maximum-product-subarray-in-an-array/
+
+// Two Traversals
 class Solution {
     public int maxProduct(int[] nums) {
         int left = nums[0], right = nums[0];
@@ -31,5 +34,24 @@ class Solution {
         
         int res = Math.max(left, right);
         return (isZero && res < 0) ? 0 : res; 
+    }
+}
+
+// Kadane's Algorithm
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = nums[0], min = nums[0], res = nums[0];
+        
+        for(int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            
+            int temp = Math.max(num, Math.max(num*max, num*min));
+            min = Math.min(num, Math.min(num*max, num*min));
+            max = temp;
+            
+            res = Math.max(res, max);
+        }
+        
+        return res;
     }
 }
